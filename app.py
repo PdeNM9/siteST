e9815cbef2230ff23a0552619a89d0f924e42b26
PdeNM9 <118948900+PdeNM9@users.noreply.github.com>
2024-03-07 18:47:17 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ import variaveis
 from st_copy_to_clipboard import st_copy_to_clipboard
 
 
+
 st.title('Consulta de Arquivos.')
 
 # Input para buscar pelo nome da minuta
 name_input = st.text_input("Digite uma palavra para pesquisar:")
 
 if name_input:
-    resultados = funcoes.buscar_minutas_por_nome(funcoes.conection, name_input)
+    resultados = funcoes.buscar_minutas_por_nome(funcoes.conexao, name_input)
     if not resultados.empty:
         # Ajuste para extrair nomes das minutas de um DataFrame
         nomes_minutas = [row.Nome_da_Minuta for row in resultados.itertuples()]
@@ -23,7 +24,8 @@ if name_input:
             row.Conteudo_da_Minuta for row in resultados.itertuples() if row.Nome_da_Minuta == escolha)
 
         # Identificar variáveis no conteúdo da minuta com base no dicionário de variáveis
-        variaveis_encontradas = {descricao: variaveis.data[descricao] for descricao in variaveis.data if variaveis.data[descricao] in conteudo_da_minuta}
+        variaveis_encontradas = {descricao: variaveis.data[descricao]
+                                 for descricao in variaveis.data if variaveis.data[descricao] in conteudo_da_minuta}
         valores_variaveis = {}
         for descricao, identificador in variaveis_encontradas.items():
             valores_variaveis[identificador] = st.sidebar.text_input(f"{descricao}:", key=identificador)
